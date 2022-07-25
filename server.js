7d7195f0fbb4bfc6f1a6588de5b99eabe3a0fae4
Felipe <pipecalgaro@gmail.com>
2022-07-25 12:07:50 -0300

    Books: create, index and new views and filtering
        modified:   .gitignore

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const expressLayouts = require('express-ejs-layouts')
 const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index')
-const authorRouter = require('./routes/author')
+const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -25,5 +26,6 @@ db.once('open', err => console.log('connected to mongoose'))
 
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
 
 app.listen(process.env.PORT || 3000)
